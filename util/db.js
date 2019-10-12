ab28d7a56053d64baf8143632077f07746ed0240
lukhee <o.balogun@ymail.com>
2019-10-12 10:34:31 +0100

    delete url mongo

--- util/db.js
+++ util/db.js
@@ -1,6 +1,3 @@
-// const url = 'mongodb://localhost:27017/beastCinemal'
-// const url = "mongodb://heroku_7r2q9thg:Balogun007.@ds333768.mlab.com:33768/heroku_7r2q9thg"
-// const url = "mongodb://balogun:Balogun007.@cluster0-shard-00-00-u8yjf.mongodb.net:27017,cluster0-shard-00-01-u8yjf.mongodb.net:27017,cluster0-shard-00-02-u8yjf.mongodb.net:27017/beastCinema?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 const url =`mongodb+srv://${process.env.MONGO_USER}:Balogun007.@cluster0-u8yjf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 
 let db
